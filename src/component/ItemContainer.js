3b2293da2f3ae749101615b9b16e00a5e5bd1456
SumiAkter2 <akteralisha36@gmail.com>
2023-08-08 09:59:56 +0600

    added multiple parameter mapDispatchToProps

--- src/component/ItemContainer.js
+++ src/component/ItemContainer.js
@@ -1,10 +1,13 @@
 // import React, { memo } from "react";
 import { connect } from "react-redux";
+import buyCake from "../Redux/Cake/CakeActions";
+import { buyChocolate } from "../Redux/Chocolate/ChocolateActions";
 
 const ItemContainer = (props) => {
   return (
     <div>
       <h1>Item : {props.item}</h1>
+      <button onClick={props.buyItem}>Buy item</button>
     </div>
   );
 };
@@ -16,4 +19,12 @@ const mapStateToProps = (state, ownProps) => {
     item: itemState,
   };
 };
-export default connect(mapStateToProps)(ItemContainer);
+const mapDispatchToProps = (dispatch, ownProps) => {
+  const dispatchFunction = ownProps.cake
+    ? () => dispatch(buyCake())
+    : () => dispatch(buyChocolate());
+  return {
+    buyItem: dispatchFunction,
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(ItemContainer);
